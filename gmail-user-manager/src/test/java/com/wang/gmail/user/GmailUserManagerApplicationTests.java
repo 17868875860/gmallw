e0bdd9f77fa6dc0c4bfd289f815f34ce42b48e09
wangruike <2596564760@qq.com>
2020-01-16 14:02:40 +0800

    提交用户管理模块

---
+++ gmail-user-manager/src/test/java/com/wang/gmail/user/GmailUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.gmail.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailUserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
